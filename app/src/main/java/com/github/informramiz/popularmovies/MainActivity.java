09b9419c39199f6258033ce1c04d0b8c77593c50
Ramiz Raja <informramiz@gmail.com>
2018-07-11 11:13:46 +0500

    feat: Write AsyncTask to fetch movies

--- app/src/main/java/com/github/informramiz/popularmovies/MainActivity.java
+++ app/src/main/java/com/github/informramiz/popularmovies/MainActivity.java
@@ -1,10 +1,17 @@
 package com.github.informramiz.popularmovies;
 
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.github.informramiz.popularmovies.model.MovieApiResponse;
+import com.github.informramiz.popularmovies.utils.JsonUtils;
 import com.github.informramiz.popularmovies.utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -17,5 +24,44 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "API key is=" + BuildConfig.API_KEY);
         NetworkUtils.buildMovieApiUrl(NetworkUtils.SORT_ORDER_TOP_RATED);
         NetworkUtils.buildImageUrl("/rv1AWImgx386ULjcf62VYaW8zSt.jpg");
+        Log.d(TAG, "Is internet present=" + NetworkUtils.isInternetPresent(this));
+        new FetchMoviesTask().execute(NetworkUtils.SORT_ORDER_POPULAR);
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, MovieApiResponse> {
+
+        @Nullable
+        @Override
+        protected MovieApiResponse doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
+
+            String sortOrder = params[0];
+            URL url = NetworkUtils.buildMovieApiUrl(sortOrder);
+            if (url == null) {
+                return null;
+            }
+
+            MovieApiResponse movieApiResponse = null;
+            try {
+                String movieApiJsonResponse = NetworkUtils.getResponseFromHttpUrl(url);
+                if (movieApiJsonResponse != null) {
+                    movieApiResponse = JsonUtils.parseMovieApiJsonResponse(movieApiJsonResponse);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return movieApiResponse;
+        }
+
+        @Override
+        protected void onPostExecute(@Nullable MovieApiResponse movieApiResponse) {
+            super.onPostExecute(movieApiResponse);
+            if (movieApiResponse != null) {
+                Log.v(TAG, "Total movies fetched=" + movieApiResponse.getResults().size());
+            }
+        }
     }
 }
