cdccd35a1619ddc1b73def0e9cadd0efadb47ed4
Ramiz Raja <informramiz@gmail.com>
2018-07-11 11:12:13 +0500

    feat: Write JSON parsing for movies response

---
+++ app/src/main/java/com/github/informramiz/popularmovies/utils/JsonUtils.java
@@ -0,0 +1,62 @@
+package com.github.informramiz.popularmovies.utils;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.github.informramiz.popularmovies.model.Movie;
+import com.github.informramiz.popularmovies.model.MovieApiResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ramiz Raja on 11/07/2018.
+ */
+public class JsonUtils {
+    @Nullable
+    public static MovieApiResponse parseMovieApiJsonResponse(@NonNull String response) {
+        MovieApiResponse movieApiResponse = new MovieApiResponse();
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            movieApiResponse.setPage(jsonObject.optInt("page"));
+            movieApiResponse.setTotalResults(jsonObject.optInt("total_results"));
+            movieApiResponse.setTotalPages(jsonObject.optInt("total_pages"));
+            JSONArray moviesJsonArray = jsonObject.optJSONArray("results");
+            movieApiResponse.setResults(parseMoviesJsonArray(moviesJsonArray));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return movieApiResponse;
+    }
+
+    private static List<Movie> parseMoviesJsonArray(JSONArray moviesJsonArray) {
+        List<Movie> movies = new ArrayList<>(moviesJsonArray.length());
+        for (int i = 0; i < moviesJsonArray.length(); i++) {
+            JSONObject movieJsonObject = moviesJsonArray.optJSONObject(i);
+            if (movieJsonObject != null) {
+                movies.add(parseMovieJsonObject(movieJsonObject));
+            }
+        }
+
+        return movies;
+    }
+
+    private static Movie parseMovieJsonObject(@NonNull JSONObject jsonObject) {
+        Movie movie = new Movie();
+
+        movie.setId(jsonObject.optInt("id"));
+        movie.setTitle(jsonObject.optString("title"));
+        movie.setPosterPath(jsonObject.optString("poster_path"));
+        movie.setVoteAverage(jsonObject.optDouble("vote_average"));
+        movie.setOverview(jsonObject.optString("overview"));
+        movie.setReleaseDate(jsonObject.optString("release_date"));
+
+        return movie;
+    }
+}
