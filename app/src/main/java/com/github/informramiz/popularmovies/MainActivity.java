6a7e87653d4c113f0177c2c811ad33355f5e2797
Ramiz Raja <informramiz@gmail.com>
2018-07-11 11:33:59 +0500

    feat: Add loading indicator and error msg views

--- app/src/main/java/com/github/informramiz/popularmovies/MainActivity.java
+++ app/src/main/java/com/github/informramiz/popularmovies/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.github.informramiz.popularmovies.model.MovieApiResponse;
 import com.github.informramiz.popularmovies.utils.JsonUtils;
@@ -16,19 +19,48 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private TextView mMoviesDataTextView;
+    private TextView mErrorMsgTextView;
+    private ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "API key is=" + BuildConfig.API_KEY);
-        NetworkUtils.buildMovieApiUrl(NetworkUtils.SORT_ORDER_TOP_RATED);
-        NetworkUtils.buildImageUrl("/rv1AWImgx386ULjcf62VYaW8zSt.jpg");
-        Log.d(TAG, "Is internet present=" + NetworkUtils.isInternetPresent(this));
-        new FetchMoviesTask().execute(NetworkUtils.SORT_ORDER_POPULAR);
+        mMoviesDataTextView = findViewById(R.id.tv_movies_data);
+        mErrorMsgTextView = findViewById(R.id.tv_error_message);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+
+        loadMoviesData();
+    }
+
+    private void loadMoviesData() {
+        if (NetworkUtils.isInternetPresent(this)) {
+            mMoviesDataTextView.setText("");
+            new FetchMoviesTask().execute(NetworkUtils.SORT_ORDER_POPULAR);
+        } else {
+            showErrorMessageView();
+        }
+    }
+
+    private void showMoviesDataView() {
+        mMoviesDataTextView.setVisibility(View.VISIBLE);
+        mErrorMsgTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showErrorMessageView() {
+        mMoviesDataTextView.setVisibility(View.INVISIBLE);
+        mErrorMsgTextView.setVisibility(View.VISIBLE);
     }
 
     public class FetchMoviesTask extends AsyncTask<String, Void, MovieApiResponse> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
 
         @Nullable
         @Override
@@ -59,8 +91,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(@Nullable MovieApiResponse movieApiResponse) {
             super.onPostExecute(movieApiResponse);
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+
             if (movieApiResponse != null) {
                 Log.v(TAG, "Total movies fetched=" + movieApiResponse.getResults().size());
+                mMoviesDataTextView.setText(String.valueOf(movieApiResponse.getResults().size()));
+                showMoviesDataView();
+            } else {
+                showErrorMessageView();
             }
         }
     }
